4beee147a5665b3eb9cf1428bf02652eea49f732
alexanderpanok <34544679+alexanderpanok@users.noreply.github.com>
2024-04-05 09:13:45 +0000

    added  bot features

--- cmd/kbot.go
+++ cmd/kbot.go
@@ -1,19 +1,28 @@
 /*
 Copyright © 2024 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
 import (
 	"fmt"
+	"log"
+	"os"
+	"time"
 
 	"github.com/spf13/cobra"
+	telebot "gopkg.in/telebot.v3"
+)
+
+var (
+	//Tele token
+	TeleToken = os.Getenv("TELE_TOKEN")
 )
 
 // kbotCmd represents the kbot command
 var kbotCmd = &cobra.Command{
-	Use:   "kbot",
-	Short: "A brief description of your command",
+	Use:     "kbot",
+	Aliases: []string{"start"},
+	Short:   "A brief description of your command",
 	Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command. For example:
 
@@ -22,6 +31,35 @@ This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("kbot called")
+		fmt.Printf("kbot %s started ", appVersion)
+		kbot, err := telebot.NewBot(telebot.Settings{
+			URL:    "",
+			Token:  TeleToken,
+			Poller: &telebot.LongPoller{Timeout: 10 * time.Second},
+		})
+		if err != nil {
+			log.Fatalf("Please check TELE_TOKEN env variable, %s", err)
+			return
+		}
+
+		kbot.Handle(telebot.OnText, func(m telebot.Context) error {
+
+			log.Print(m.Message().Payload, m.Text())
+
+			payload := m.Message().Payload
+
+			switch payload {
+			case "hello":
+				err = m.Send(fmt.Sprintf("Hello, I'm kbot %s", appVersion))
+			case "cat":
+				err = m.Send(fmt.Sprintf("I like cats"))
+			}
+			return err
+
+		})
+
+		kbot.Start()
+
 	},
 }
 
